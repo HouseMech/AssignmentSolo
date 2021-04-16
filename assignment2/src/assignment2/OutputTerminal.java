220acadcf3bd70080ede5e7bd132afad2ee47be3
Liam Harbec <liam.harbec@gmail.com>
2021-04-16 09:15:18 -0700

    Added google translate function. Requires use of an API key

--- assignment2/src/assignment2/OutputTerminal.java
+++ assignment2/src/assignment2/OutputTerminal.java
@@ -1,6 +1,8 @@
 package assignment2;
 
 import java.awt.Color;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
@@ -24,13 +26,26 @@ public class OutputTerminal extends JTextArea{
 	 * takes in String send it to chat bot then prints both input String and chatBot output in Terminal area
 	 */
 	public void askQuestion(String s) {
+		String translated = "";
 		if(s.length() != 0) {
-			String response = chatbot.getResponse(s);
-			setText(getText() + "\n human: " + s);
-			setText(getText() + "\n Bot: " + response + "\n");
-			POSTagger.checkUserInput(s);
+			try {
+				translated = TranslateToEnglish.translate(s).toLowerCase();
+			} catch (IOException | GeneralSecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (translated.length() > 0) {
+				String response = chatbot.getResponse(translated);
+				setText(getText() + "\n human: " + s);
+				setText(getText() + "\n Bot: " + response + "\n");
+				POSTagger.checkUserInput(translated);
+			}
+			else {
+				String response = chatbot.getResponse(s);
+				setText(getText() + "\n human: " + s);
+				setText(getText() + "\n Bot: " + response + "\n");
+				POSTagger.checkUserInput(s);
+			}
 		}
 	}
-	
-
 }
